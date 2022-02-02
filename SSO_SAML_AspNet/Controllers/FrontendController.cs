4f82de181530e2750a677042e3d7fce1ebcf950c
Ajith Srikukan <a.srikukan@sana-commerce.com>
2022-02-02 20:29:35 +0530

    SSO with SAML for admin/frontend login

---
+++ SSO_SAML_AspNet/Controllers/FrontendController.cs
@@ -0,0 +1,57 @@
+﻿using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.Mvc;
+using ITfoxtec.Identity.Saml2.Schemas;
+using SSO_SAML_AspNet.Helper;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Web.Mvc;
+
+namespace SSO_SAML_AspNet.Controllers
+{
+    public class FrontendController : Controller
+    {
+        const string relayStateReturnUrl = "ReturnUrl";
+        const string assertionConsumerServiceUrl = "https://localhost:44370/frontend/LoginAssertionConsumerService";
+
+        public ActionResult Index(string name)
+        {
+            return View("Index", null, name);
+        }
+
+        [HttpGet]
+        public ActionResult Login(string returnUrl = null)
+        {
+            var config = (new IdentityConfigHelper()).GetSAMLConfig();
+
+            var binding = new Saml2RedirectBinding();
+            binding.SetRelayStateQuery(new Dictionary<string, string> { { relayStateReturnUrl, returnUrl ?? Url.Content("~/") } });
+
+            return binding.Bind(new Saml2AuthnRequest(config)
+            {
+                AssertionConsumerServiceUrl = new System.Uri(assertionConsumerServiceUrl)
+            }).ToActionResult();
+        }
+
+        [HttpPost]
+        public ActionResult LoginAssertionConsumerService()
+        {
+            var config = (new IdentityConfigHelper()).GetSAMLConfig();
+
+            var binding = new Saml2PostBinding();
+            var saml2AuthnResponse = new Saml2AuthnResponse(config);
+
+            binding.ReadSamlResponse(Request.ToGenericHttpRequest(), saml2AuthnResponse);
+            if (saml2AuthnResponse.Status != Saml2StatusCodes.Success)
+            {
+                throw new AuthenticationException($"SAML Response status: {saml2AuthnResponse.Status}");
+            }
+            binding.Unbind(Request.ToGenericHttpRequest(), saml2AuthnResponse);
+
+            //saml2AuthnResponse.CreateSession(claimsAuthenticationManager: new DefaultClaimsAuthenticationManager());
+
+            var relayStateQuery = binding.GetRelayStateQuery();
+            var returnUrl = relayStateQuery.ContainsKey(relayStateReturnUrl) ? relayStateQuery[relayStateReturnUrl] : Url.Content("~/");
+            return RedirectToAction("Index", new { name = saml2AuthnResponse.ClaimsIdentity.Name });
+        }
+    }
+}
---
+++ SSO_SAML_AspNet/Helper/IdentityConfigHelper.cs
@@ -0,0 +1,81 @@
+﻿using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.Schemas.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Security;
+using ITfoxtec.Identity.Saml2.Util;
+using System.IdentityModel.Claims;
+using System.Web.Helpers;
+using System.Web;
+
+namespace SSO_SAML_AspNet.Helper
+{
+    public class IdentityConfigHelper
+    {
+        public  Saml2Configuration GetSAMLConfig1()
+        {
+            var config = new Saml2Configuration();
+            config.Issuer = ConfigurationManager.AppSettings["Saml2:Issuer"];
+            config.AllowedAudienceUris.Add(config.Issuer);
+
+            config.CertificateValidationMode = X509CertificateValidationMode.ChainTrust;
+            config.RevocationMode = X509RevocationMode.NoCheck;
+
+            var entityDescriptor = new EntityDescriptor();
+            //entityDescriptor.ReadIdPSsoDescriptorFromUrl(new Uri("https://dev-30220723.okta.com/app/exk3qrnqbrhJVPLOE5d7/sso/saml/metadata"));
+            entityDescriptor.ReadIdPSsoDescriptorFromUrl(new Uri(ConfigurationManager.AppSettings["Saml2:IdPMetadata"]));
+            if (entityDescriptor.IdPSsoDescriptor != null)
+            {
+                config.SingleSignOnDestination = entityDescriptor.IdPSsoDescriptor.SingleSignOnServices.First().Location;
+                config.SignatureValidationCertificates.AddRange(entityDescriptor.IdPSsoDescriptor.SigningCertificates);
+            }
+            else
+            {
+                throw new Exception("IdPSsoDescriptor not loaded from metadata.");
+            }
+
+            return config;
+        }
+
+        public Saml2Configuration GetSAMLConfig()
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+
+            var config = new Saml2Configuration
+            {
+                Issuer = ConfigurationManager.AppSettings["Saml2:Issuer"]
+            };
+            config.AllowedAudienceUris.Add(config.Issuer);
+
+            // additional settting if you want to use certificated to validate the response.
+            //config.SignatureAlgorithm = ConfigurationManager.AppSettings["Saml2:SignatureAlgorithm"];
+            //config.SigningCertificate = CertificateUtil.Load(HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["Saml2:SigningCertificateFile"]), ConfigurationManager.AppSettings["Saml2:SigningCertificatePassword"]);
+
+            config.CertificateValidationMode = (X509CertificateValidationMode)Enum.Parse(typeof(X509CertificateValidationMode), ConfigurationManager.AppSettings["Saml2:CertificateValidationMode"]);
+            config.RevocationMode = (X509RevocationMode)Enum.Parse(typeof(X509RevocationMode), ConfigurationManager.AppSettings["Saml2:RevocationMode"]);
+
+
+            var entityDescriptor = new EntityDescriptor();
+            entityDescriptor.ReadIdPSsoDescriptorFromUrl(new Uri(ConfigurationManager.AppSettings["Saml2:IdPMetadata"]));
+            if (entityDescriptor.IdPSsoDescriptor != null)
+            {
+                config.AllowedIssuer = entityDescriptor.EntityId;
+                config.SingleSignOnDestination = entityDescriptor.IdPSsoDescriptor.SingleSignOnServices.First().Location;
+                config.SignatureValidationCertificates.AddRange(entityDescriptor.IdPSsoDescriptor.SigningCertificates);
+                if (entityDescriptor.IdPSsoDescriptor.WantAuthnRequestsSigned.HasValue)
+                {
+                    config.SignAuthnRequest = entityDescriptor.IdPSsoDescriptor.WantAuthnRequestsSigned.Value;
+                }
+            }
+            else
+            {
+                throw new Exception("IdPSsoDescriptor not loaded from metadata.");
+            }
+
+            return config;
+        }
+    }
+}
